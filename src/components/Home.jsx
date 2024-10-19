6e081fdbc04a68e02644c7b5fa46d05c60873c0f
Didar-jpg <guneshev.didar@gmail.com>
2024-10-19 17:37:10 +0500

    Addede Cards in Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,71 @@
 import React from 'react'
+
+import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import viceCity from '../images/gta_vice_city.jpg'
+
 import Slider from './Slider.jsx'
 
 export default function Home() {
   return (  
-    <Slider/>
+    <>
+          <Slider/>
+
+          <Container className='py-5'>
+            <Row>
+              <Col>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={viceCity} className='w-100'/>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={viceCity} className='w-100'/>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={viceCity} className='w-100'/>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={viceCity} className='w-100'/>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+    </>
   )
 }
